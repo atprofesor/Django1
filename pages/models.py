cf17dda9cf5f14aa12637e10fb4df115dd74829e
atprofesor <atprofesor@gmail.com>
2026-03-30 23:25:14 -0400

    Admin and Models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200) # Un título corto
+    content = models.TextField()               # Un texto largo
+
+    def __str__(self):
+        return self.title # Esto hace que en el admin se vea el título y no "Post object"
