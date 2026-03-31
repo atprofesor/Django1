9e6e29cac9337b07e63b8c0a808d68d2a4545066
atprofesor <atprofesor@gmail.com>
2026-03-30 23:52:15 -0400

    Post-List en Home

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = "home.html"
+    context_object_name = "all_posts_list"
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
